2a9dc6babf9f063f949cc5f57dc4ac299542f0a5
Yashwanth661 <yashwanth661@gmail.com>
2026-02-13 00:00:28 +0530

    Removed bootstrap scripts

--- scripts/ubuntu-bootstrap-setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install git
-git clone https://github.com/Yashwanth661/fresh-canvas.git ~/dotfiles
-cd ~/dotfiles/scripts
-chmod +x bootstrap-all.sh ubuntu-apps-install.sh setup-fonts.sh
-./bootstrap-all.sh
